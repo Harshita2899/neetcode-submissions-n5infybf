4d3f06736c225d482ec39a45b0f369ceb21c69bf
Harshita2899 <66833153+Harshita2899@users.noreply.github.com>
2026-05-13 08:00:40 +0530

    Add: matchsticks-to-square - submission-1

---
+++ Data Structures & Algorithms/matchsticks-to-square/submission-1.py
@@ -0,0 +1,23 @@
+class Solution:
+    def chkValid(self,indx,matchsticks):
+        if indx==len(matchsticks) and sum(self.side)//4==self.sideLength:
+            return True
+        for i in range(4):
+            if self.side[i]+matchsticks[indx]<=self.sideLength:
+                self.side[i]=self.side[i]+matchsticks[indx]
+                if self.chkValid(indx+1,matchsticks):
+                    return True
+                self.side[i]=self.side[i]-matchsticks[indx]
+        return False
+        
+    def makesquare(self, matchsticks: List[int]) -> bool:
+        self.sideLength = sum(matchsticks)//4
+        self.side=[0]*4
+        if self.sideLength != sum(matchsticks)/4:
+            return False
+        matchsticks.sort(reverse=True)
+
+        if matchsticks[0]>self.sideLength:
+            return False
+
+        return self.chkValid(0,matchsticks)
